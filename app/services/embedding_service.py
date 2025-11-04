34bdcc31d163155a9c021b4b5edfe38525cbbe46
dagi <dagiteferi2011@gmail.com>
2025-11-04 18:19:24 +0300

    update embedder

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -4,7 +4,7 @@ import os
 
 class LocalEmbeddingService:
     def __init__(self):
-        model_path = os.getenv("SENTENCE_TRANSFORMER_MODEL_PATH", 'paraphrase-multilingual-MiniLM-L12-v2')
+        model_path = os.getenv("SENTENCE_TRANSFORMER_MODEL_PATH", 'all-MiniLM-L6-v2')
         # If model_path is a local directory, load from there. Otherwise, SentenceTransformer will download.
         self.model = SentenceTransformer(model_path)
         self.embedding_dim = 384  # Dimension for chosen model
